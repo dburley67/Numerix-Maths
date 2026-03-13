0b466236841d01e5443b88d0b63ae402806f6ac9
Alex14up <alex.ajcda@gmail.com>
2026-03-13 18:36:22 +0000

    Lesson 1, Lesson 2 and Lesson 3
    A collection of maths revision webpages covering decimals & rounding, factors & multiples, HCF, and standard form.
    Router's index.js has been updated as well.

---
+++ index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import WelcomePage from "../pages/WelcomePage.vue";
+import HomePage from "../pages/HomePage.vue";
+import LessonsPage from "../pages/LessonsPage.vue";
+import PracticePage from "../pages/PracticePage.vue";
+import AchievementsPage from "../pages/AchievementsPage.vue";
+import GamesPage from "../pages/GamesPage.vue";
+import SettingsPage from "../pages/SettingsPage.vue";
+import ContactPage from "../pages/ContactPage.vue";
+import TeamPage from "../pages/TeamPage.vue";
+import Lesson1 from "../Lessons/Lesson1.vue";
+import Lesson2 from "../Lessons/Lesson2.vue";
+import Lesson3 from "../Lessons/Lesson3.vue";
+import { patchProp } from "vue";
+
+const routes = [
+  { path: "/", component: WelcomePage, meta: { layout: "welcome" } },
+
+  { path: "/home", component: HomePage },
+  { path: "/lessons", component: LessonsPage },
+  { path: "/practice", component: PracticePage },
+  { path: "/achievements", component: AchievementsPage },
+  { path: "/games", component: GamesPage },
+  { path: "/settings", component: SettingsPage },
+  { path: "/contact", component: ContactPage },
+  { path: "/team", component: TeamPage },
+  { path: "/Lesson1", component: Lesson1},
+  { path: "/Lesson2", component: Lesson2},
+  { path: "/Lesson3", component: Lesson3}
+];
+
+export default createRouter({
+  history: createWebHistory(),
+  routes,
+});
