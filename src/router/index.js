983009272e880f36f15632b8da8fb20cb4dec976
kl680student <kl680.student.aru@gmail.com>
2026-03-26 01:53:23 +0000

    Update index.js in router
    All pages should be linked

--- src/router/index.js
+++ src/router/index.js
@@ -12,11 +12,14 @@ import TeamPage from "../pages/TeamPage.vue";
 import Lesson1 from "../pages/DecimalRounding.vue";
 import Lesson2 from "../pages/FactorMultiplesPrimes.vue";
 import Lesson3 from "../pages/StandardForm.vue";
-
+import Lesson4 from "../pages/PercentageFinance.vue";
+import Lesson5 from "../pages/FinanceLesson.vue"
+import Lesson6 from "../pages/BasicOperations.vue"
+import Lesson7 from "../pages/BIDMAS.vue"
+import Lesson8 from "../pages/NegativeNumbers.vue"
 
 const routes = [
   { path: "/", component: WelcomePage, meta: { layout: "welcome" } },
-
   { path: "/home", component: HomePage },
   { path: "/lessons", component: LessonsPage },
   { path: "/practice", component: PracticePage },
@@ -27,7 +30,12 @@ const routes = [
   { path: "/team", component: TeamPage },
   { path: "/Lesson1", component: Lesson1},
   { path: "/Lesson2", component: Lesson2},
-  { path: "/Lesson3", component: Lesson3}
+  { path: "/Lesson3", component: Lesson3},
+  { path: "/Lesson4", component: Lesson4},
+  { path: "/Lesson5", component: Lesson5},,
+  { path: "/Lesson6", component: Lesson6},
+  { path: "/Lesson7", component: Lesson7},
+  { path: "/Lesson8", component: Lesson8}
 ];
 
 export default createRouter({
