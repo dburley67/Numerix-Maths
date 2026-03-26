93bcfc4a77124d8c1e5d823e867240756ec8d09f
kl680student <kl680.student.aru@gmail.com>
2026-03-26 02:43:53 +0000

    Index Update

--- src/router/index.js
+++ src/router/index.js
@@ -13,8 +13,10 @@ import Lesson1 from "../pages/DecimalRounding.vue";
 import Lesson2 from "../pages/FactorMultiplesPrimes.vue";
 import Lesson3 from "../pages/StandardForm.vue";
 import Lesson4 from "../pages/PercentageFinance.vue";
-import Lesson5 from "../pages/FinanceLesson.vue";
-import Lesson7 from "../pages/BIDMAS.vue";
+import Lesson5 from "../pages/FinanceLesson.vue"
+import Lesson6 from "../pages/BasicOperations.vue"
+import Lesson7 from "../pages/BIDMAS.vue"
+import Lesson8 from "../pages/NegativeNumbers.vue"
 
 const routes = [
   { path: "/", component: WelcomePage, meta: { layout: "welcome" } },
@@ -26,13 +28,14 @@ const routes = [
   { path: "/settings", component: SettingsPage },
   { path: "/contact", component: ContactPage },
   { path: "/team", component: TeamPage },
-  { path: "/Lesson1", component: Lesson1 },
-  { path: "/Lesson2", component: Lesson2 },
-  { path: "/Lesson3", component: Lesson3 },
-  { path: "/Lesson4", component: Lesson4 },
-  { path: "/Lesson5", component: Lesson5 },
-  ,
-  { path: "/Lesson7", component: Lesson7 },
+  { path: "/Lesson1", component: Lesson1},
+  { path: "/Lesson2", component: Lesson2},
+  { path: "/Lesson3", component: Lesson3},
+  { path: "/Lesson4", component: Lesson4},
+  { path: "/Lesson5", component: Lesson5},,
+  { path: "/Lesson6", component: Lesson6},
+  { path: "/Lesson7", component: Lesson7},
+  { path: "/Lesson8", component: Lesson8}
 ];
 
 export default createRouter({
